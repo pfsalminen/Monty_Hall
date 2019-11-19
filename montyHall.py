ffa56f9e86d31187b344660d60217a9004c497e6
I23558 <psalminen@verisk.com>
2019-11-19 16:20:04 -0700

    / ‘montyHall.py’

--- montyHall.py
+++ montyHall.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 """ Monty Hall Test Program
 Author: Paul Salminen
 Date: 11/12/2015
@@ -7,39 +6,32 @@ Date: 11/12/2015
 
 import random
 
+
 def run():
-	total = 1000000
-	fWins = 0
-	sWins = 0
-	opts = [0,1,2]
+    total = 1000000
+    fWins = 0
+    sWins = 0
+    opts = [0, 1, 2]
 
-	for _ in range(total):
-		answer = random.randint(0,2)
-		fGuess = random.randint(0,2)
-		clue = [
-			i 
-			for i in opts
-			if i != fGuess 
-			and i!=answer
-		][0]
-		sGuess = [
-			i 
-			for i in opts 
-			if i != fGuess 
-			and i!=clue
-		][0]
-		
-		if fGuess == answer:
-			fWins+=1
-		elif sGuess == answer:
-			sWins += 1
-		else:
-			print("Error!")
+    for _ in range(total):
+        answer = random.randint(0, 2)
+        fGuess = random.randint(0, 2)
+        clue = [i for i in opts if i != fGuess and i != answer][0]
+        sGuess = [i for i in opts if i != fGuess and i != clue][0]
 
-	first_guess = float(fWins)*100 / float(total)
-	second_guess = float(sWins)*100 / float(total)
+        if fGuess == answer:
+            fWins += 1
+        elif sGuess == answer:
+            sWins += 1
+        else:
+            print("Error!")
 
-	print(f'First Guess: {first_guess:.2f}% \nSecond Guess: {second_guess:.2f}%')
+    first_guess = float(fWins) * 100 / float(total)
+    second_guess = float(sWins) * 100 / float(total)
 
-if __name__=='__main__':
-	run()
+    print(
+        f'First Guess: {first_guess:.2f}% \nSecond Guess: {second_guess:.2f}%')
+
+
+if __name__ == '__main__':
+    run()
